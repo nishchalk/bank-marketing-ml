673fd9a0f857360f9803fda6ec20474e3e697d8f
n0k00km <nishchal.kutarekar@walmart.com>
2026-02-09 17:19:17 +0530

    fix: wide notebook view

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ if page == "Training Notebook":
     with open("Bank_Marketing_Assignment_2.html", "r", encoding="utf-8") as f:
         html_content = f.read()
     # Display the HTML content
-    st.components.v1.html(html_content, width=800, height=600, scrolling=True)
+    st.components.v1.html(html_content, width="stretch")
     # with open("Bank_Marketing_Assignment_2.ipynb", "r", encoding="utf-8") as f:
     #     st.code(f.read(), language="python")
 
